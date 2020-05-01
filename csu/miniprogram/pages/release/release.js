b569eb8a892eaa07425ba486144b26348c7e8db9
dhao2001 <dhao2001@outlook.com>
2020-05-01 20:40:19 +0800

    Change Release Page by XCL. Need to merge CAREFULLY!

--- csu/miniprogram/pages/release/release.js
+++ csu/miniprogram/pages/release/release.js
@@ -1,16 +1,15 @@
 // pages/release/release.js
+const db = wx.cloud.database()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    time:false,
-    calendar: [],
-    width: 0,
-    currentIndex: 0,
-    currentTime: 0,
-    timeArr: [{ "time": "8:00-10:00", "status": "约满" }, { "time": "8:00-10:00", "status": "约满" }, { "time": "8:00-10:00", "status": "约满" }, { "time": "8:00-10:00", "status": "约满" }, { "time": "8:00-10:00", "status": "约满" }, { "time": "8:00-10:00", "status": "约满" }, { "time": "8:00-10:00", "status": "约满" }, { "time": "8:00-10:00", "status": "约满" }, { "time": "8:00-10:00", "status": "约满" }, { "time": "8:00-10:00", "status": "约满" }, { "time": "8:00-10:00", "status": "约满" }, { "time": "8:00-10:00", "status": "约满" }, { "time": "8:00-10:00", "status": "约满" }],
+    name:'',
+    introduction:'',
+    sport_title:'',
+    loadingHidden:true,
     location:"点击获取地理位置",
     level:'选择等级',
     //发起运动的文字内容
@@ -62,70 +61,58 @@ Page({
   showPopup() {
     this.setData({ show: true });
   },
-  choose_time(){
-    this.setData({ time: true });
-  },
+
   onClose() {
-    this.setData({ show: false, time: false });
+    this.setData({ show: false });
   },
   /**
    * 生命周期函数--监听页面加载
    */
+  submit:function(e){
+    var that = this
+    that.setData({
+      loadingHidden:false
+    })
+    setTimeout(function(){
+      that.setData({
+        loadingHidden: true
+      })
+      db.collection('sport').add({
+        data:{
+          _sport_title:that.data.sport_title,
+          _introduction:that.data.introduction,
+          _name:that.data.name,
+          _time:'未定'
+        },
+        success: res => {
+          console.log("成功添加运动信息！")
+          
+        }
+      })
+      wx.switchTab({
+        url: '/pages/index/index',
+      })
+      
+    },1000)
+},
+
+  intro_change:function(e){
+    this.setData({
+      introduction:e.detail.value
+    })
+  },
+  name_change:function(e){
+    this.setData({
+      name: e.detail.value
+    })
+  },
+  sport_title_change:function(e){
+    this.setData({
+      sport_title: e.detail.value
+    })
+  },
   onLoad: function (options) {
-    var that = this;
-    function getThisMonthDays(year, month) {
-      return new Date(year, month, 0).getDate();
-    }
-    // 计算每月第一天是星期几
-    function getFirstDayOfWeek(year, month) {
-      return new Date(Date.UTC(year, month - 1, 1)).getDay();
-    }
-    const date = new Date();
-    const cur_year = date.getFullYear();
-    const cur_month = date.getMonth() + 1;
-    const cur_date = date.getDate();
-    const weeks_ch = ['日', '一', '二', '三', '四', '五', '六'];
-    //利用构造函数创建对象
-    console.log(date);
-    var monthLength = getThisMonthDays(cur_year, cur_month)
-    function calendar(date, week) {
-      if (i > monthLength){
-        this.date = cur_year + '-' + (parseInt(cur_month) + 1) + '-' + date % monthLength;
-      }
-      else{
-        this.date = cur_year + '-' + cur_month + '-' + date;
-      }
-      if(date==cur_date){
-        this.week = "今天";
-      }else if(date==cur_date+1){
-        this.week = "明天";
-      }else{
-        this.week = '星期' + week;
-      }
-    }
-    //当前月份的天数
-   
-    //当前月份的第一天是星期几
-    var week = getFirstDayOfWeek(cur_year, cur_month)
-    var x = week;
-    for (var i = 1; i <= monthLength+7; i++) {
-      //当循环完一周后，初始化再次循环
-      if (x > 6) {
-        x = 0;
-      }
-      //利用构造函数创建对象
-      that.data.calendar[i] = new calendar(i, [weeks_ch[x]][0])
-      x++;
-    }
-    //限制要渲染的日历数据天数为7天以内（用户体验）
-    var flag = that.data.calendar.splice(cur_date, 7)
-    that.setData({
-      calendar: flag
-    })
-    //设置scroll-view的子容器的宽度
-    that.setData({
-      width: 186 * parseInt(7)
-    })
+    var that = this
     // console.log(app.globalData)
     wx.getSystemInfo({
       success: function (res) {
@@ -186,17 +173,5 @@ Page({
    */
   onShareAppMessage: function () {
 
-  },
-  select: function (event) {
-    //为上半部分的点击事件
-    this.setData({
-      currentIndex: event.currentTarget.dataset.index
-    })
-  },
-  selectTime: function (event) {
-    //为下半部分的点击事件
-    this.setData({
-      currentTime: event.currentTarget.dataset.tindex
-    })
   }
 })
