facda00c931d94be9b6716f9a1aeff6109ea6f40
Your Name <1185148797@qq.com>
2020-05-17 00:43:38 +0800

    ani

--- csu/miniprogram/pages/me/me.js
+++ csu/miniprogram/pages/me/me.js
@@ -8,34 +8,54 @@ Page({
     username: '加载中',
     score: 0,
     logthree: [],
-    my_release:false,//控制我发起的
-    my_release_detail: ["try"],//控制我发起的
-    my_join: false,//控制我我参加的
-    my_join_detail: ["try","try"],//控制我我参加的
-    touch_times_release: 1,//控制我发起的
-    touch_times_join: 1,//控制我参加的
-    pop_detail_release: false,//控制我发起
-    pop_detail_join: false,//控制我参加
+    my_release: false, //控制我发起的
+    my_release_detail: ["try"], //控制我发起的
+    my_join: false, //控制我我参加的
+    my_join_detail: ["try", "try"], //控制我我参加的
+    touch_times_release: 1, //控制我发起的
+    touch_times_join: 1, //控制我参加的
+    pop_detail_release: false, //控制我发起
+    pop_detail_join: false, //控制我参加
   },
-  join_hidden_change: function (e) {//控制我参加
+  join_hidden_change: function(e) { //控制我参加
     var that = this
     var touch_times_join = that.data.touch_times_join
+    var animation_join = wx.createAnimation({
+      duration: 400,
+      timingFunction: 'ease',
+      delay: 0
+    });
+    var animation_join1 = wx.createAnimation({
+      duration: 400,
+      timingFunction: 'ease',
+      delay: 0
+    });
+    animation_join.translateY(-335).step();
+    animation_join1.translateY(0).step();
     that.setData({
       touch_times_join: touch_times_join + 1
     })
     if (that.data.touch_times_join % 2 == 0) {
+      setTimeout(function () {
+        that.setData({
+          my_join: true
+        })
+      }, 400)
       that.setData({
-        my_join: true
+        ani_join: animation_join.export(),
       })
-
-
     } else {
-      this.setData({
-        my_join: false
+
+        that.setData({
+          my_join: false
+        })
+
+      that.setData({
+        ani_join: animation_join1.export(),
       })
     }
   },
-  release_hidden_change: function (e) {//控制我发起的
+  release_hidden_change: function(e) { //控制我发起的
     var that = this
     var touch_times_release = that.data.touch_times_release
     that.setData({
@@ -53,30 +73,30 @@ Page({
       })
     }
   },
-  match_detail_release: function (e) {//控制release
+  match_detail_release: function(e) { //控制release
     this.setData({
       pop_detail_release: true
     })
     console.log('开启')
   },
-  pop_fade2_release: function (e) {//控制release
+  pop_fade2_release: function(e) { //控制release
     var that = this
-    setTimeout(function () {
+    setTimeout(function() {
       that.setData({
         pop_detail_release: false
       })
     }, 100)
     console.log('关闭')
   },
-  match_detail_join: function (e) {//控制join
+  match_detail_join: function(e) { //控制join
     this.setData({
       pop_detail_join: true
     })
     console.log('开启')
   },
-  pop_fade2_join: function (e) {//控制join
+  pop_fade2_join: function(e) { //控制join
     var that = this
-    setTimeout(function () {
+    setTimeout(function() {
       that.setData({
         pop_detail_join: false
       })
@@ -85,35 +105,47 @@ Page({
   },
 
 
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this
     this.setData({
       AvatarUrl: "https://6665-feifeiniubi-cmo2o-1301607192.tcb.qcloud.la/testava.jpg?sign=6fdab6348eeeeecc9ae14d7c992abb03&t=1588086430",
-      
-      logthree: [{'date':'2.30','proj':'步行10公里','change':'+10'},{'date':'3.32','proj':'长跑5公里','change':'+15'},{'date':'4.31','proj':'步行10公里','change':'+10'}]
+
+      logthree: [{
+        'date': '2.30',
+        'proj': '步行10公里',
+        'change': '+10'
+      }, {
+        'date': '3.32',
+        'proj': '长跑5公里',
+        'change': '+15'
+      }, {
+        'date': '4.31',
+        'proj': '步行10公里',
+        'change': '+10'
+      }]
     })
     db.collection('person_message').doc('19762d645eae6142004ed6e32b6e4da4').get({
-      success:res =>{
+      success: res => {
         that.setData({
-          score:res.data._score
+          score: res.data._score
         })
       }
     })
-  //这里你要加个初始化join的
+    //这里你要加个初始化join的
     db.collection('sport').where({
-      _openid: app.appData.user_openid
-    })
-    .get({
-      success:res =>{
-        console.log(res.data)
-        that.setData({
-          my_release_detail:res.data
-        })
-      }
-    })
+        _openid: app.appData.user_openid
+      })
+      .get({
+        success: res => {
+          console.log(res.data)
+          that.setData({
+            my_release_detail: res.data
+          })
+        }
+      })
     db.collection('Participate').where({
-      _openid: app.appData.user_openid
-    })
+        _openid: app.appData.user_openid
+      })
       .get({
         success: res => {
           console.log(res.data)
@@ -123,7 +155,7 @@ Page({
         }
       })
   },
-  onShow: function (options) {
+  onShow: function(options) {
     db.collection('person_message').doc('19762d645eae6142004ed6e32b6e4da4').get({
       success: res => {
         that.setData({
@@ -135,6 +167,6 @@ Page({
     console.log('切换成功')
     console.log(app.appData.user_openid)
   },
-  
-  
+
+
 })
