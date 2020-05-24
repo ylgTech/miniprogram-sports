fa7a99787b6f703e8182c0d5028a1aa3c3337b83
Your Name <1185148797@qq.com>
2020-05-24 16:26:55 +0800

    wdl

--- csu/miniprogram/pages/index/index.js
+++ csu/miniprogram/pages/index/index.js
@@ -41,7 +41,7 @@ Page({
     nextMonth: 'nextMonth',
     selectVal: '',
     imgUrls: [
-      "https://656e-energycsu-x8fn6-1301628535.tcb.qcloud.la/%E5%9B%BE%E7%89%87/kalen-emsley-kGSapVfg8Kw-unsplash.jpg?sign=a076fa7a17b73ee4650a83c5244efbe9&t=1590280808", "https://656e-energycsu-x8fn6-1301628535.tcb.qcloud.la/%E5%9B%BE%E7%89%87/noah-buscher-jyQChhw-WbI-unsplash.jpg?sign=62de79f3fcb38cbc26c355f330d1a9af&t=1590280867", "https://656e-energycsu-x8fn6-1301628535.tcb.qcloud.la/%E5%9B%BE%E7%89%87/peter-osmenda-20K-JZ2ppbE-unsplash.jpg?sign=9a40bff0da376a6b131076654cd01371&t=1590280880",
+      "https://656e-energycsu-x8fn6-1301628535.tcb.qcloud.la/%E5%9B%BE%E7%89%87/kalen-emsley-kGSapVfg8Kw-unsplash.jpg?sign=a076fa7a17b73ee4650a83c5244efbe9&t=1590280808", "https://656e-energycsu-x8fn6-1301628535.tcb.qcloud.la/%E5%9B%BE%E7%89%87/noah-buscher-jyQChhw-WbI-unsplash.jpg?sign=62de79f3fcb38cbc26c355f330d1a9af&t=1590280867", "https://656e-energycsu-x8fn6-1301628535.tcb.qcloud.la/%E5%9B%BE%E7%89%87/gustavo-torres-JwYsUx7JyuA-unsplash.jpg?sign=50f4ea269e23d8fe99c4ba9786df9093&t=1590308707",
     ],
     rank: [{
         rank: 1,
