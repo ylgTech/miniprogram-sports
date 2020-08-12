3f7295417bb95b590b28eb4e5e1936c95912aa60
Your Name <1185148797@qq.com>
2020-08-12 17:05:38 +0800

    排行榜

--- csu/miniprogram/pages/index/index.js
+++ csu/miniprogram/pages/index/index.js
@@ -11,19 +11,12 @@ var qqmapsdk = new QQMapWX({
 
 });
 Page({
-  onLoad: function() {
-
-    this.setData({
-      height: wx.getSystemInfoSync().windowHeight,
-      width: wx.getSystemInfoSync().windowWidth
-    })
-
-  }, //获取页面长度,高度
+  //获取页面长度,高度
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: null,
     distance: '',
     latitude: '',
     longitude: '',
@@ -48,13 +41,13 @@ Page({
     lastMonth: 'lastMonth',
     nextMonth: 'nextMonth',
     selectVal: '',
-    imgUrlPre:'https://656e-energycsu-x8fn6-1301628535.tcb.qcloud.la/%E5%9B%BE%E7%89%87/1.jpg?sign=aefe7511f2365d559f373af6d045e2f6&t=1595732807',
+    imgUrlPre: 'https://656e-energycsu-x8fn6-1301628535.tcb.qcloud.la/%E5%9B%BE%E7%89%87/1.jpg?sign=aefe7511f2365d559f373af6d045e2f6&t=1595732807',
     imgUrls: [
       "https://656e-energycsu-x8fn6-1301628535.tcb.qcloud.la/%E5%9B%BE%E7%89%87/kalen-emsley-kGSapVfg8Kw-unsplash.jpg?sign=a076fa7a17b73ee4650a83c5244efbe9&t=1590280808", "https://656e-energycsu-x8fn6-1301628535.tcb.qcloud.la/%E5%9B%BE%E7%89%87/noah-buscher-jyQChhw-WbI-unsplash.jpg?sign=62de79f3fcb38cbc26c355f330d1a9af&t=1590280867", "https://656e-energycsu-x8fn6-1301628535.tcb.qcloud.la/%E5%9B%BE%E7%89%87/kate-m-O0x4a5pJP0M-unsplash.jpg?sign=9404a6f424e10025bb52163812cbe87e&t=1590308897",
     ],
-    img:'https://656e-energycsu-x8fn6-1301628535.tcb.qcloud.la/%E5%9B%BE%E7%89%87/1.jpg?sign=aefe7511f2365d559f373af6d045e2f6&t=1595732807',
+    img: 'https://656e-energycsu-x8fn6-1301628535.tcb.qcloud.la/%E5%9B%BE%E7%89%87/1.jpg?sign=aefe7511f2365d559f373af6d045e2f6&t=1595732807',
   },
-  select: function(e) {
+  select: function (e) {
     this.setData({
       selectVal: e.detail
     })
@@ -64,7 +57,7 @@ Page({
   },
 
   //活动点击事件，显示详情页
-  match_detail: function(e) {
+  match_detail: function (e) {
     var that = this
     db.collection('Participate').where({
         activity_id: that.data.match_all[e.currentTarget.id]._id,
@@ -80,7 +73,7 @@ Page({
             })
           }
           that.setData({
-            participateId:res.data[0]._id
+            participateId: res.data[0]._id
           })
         }
       })
@@ -98,7 +91,7 @@ Page({
     //       }
     //     }
     //   })
-    if ( that.data.pop_btn_in == false) {
+    if (that.data.pop_btn_in == false) {
       that.setData({
         pop_btn: true
       })
@@ -114,19 +107,20 @@ Page({
 
     console.log('开启')
   },
-  formSubmit(e){
-    var _this=this;
+  formSubmit(e) {
+    var _this = this;
     qqmapsdk.reverseGeocoder({
-      location:'',
-      success:function(res){
+      location: '',
+      success: function (res) {
         console.log(res);
-      },fail:function(error){
+      },
+      fail: function (error) {
         console.error(error)
       }
     })
   },
   //参与运动点击事件
-  pop_fade: function(e) {
+  pop_fade: function (e) {
     var that = this
     var detailIndex = that.data.detailIndex
     var match_all = that.data.match_all
@@ -134,8 +128,8 @@ Page({
       data: {
         // _if_finished: false,
         activity_id: match_all[detailIndex]._id,
-        status:'sign pu',
-        user_id:''
+        status: 'sign pu',
+        user_id: ''
       },
       success: {}
     })
@@ -144,8 +138,8 @@ Page({
       duration: 2000, //显示时长
       mask: true, //是否显示透明蒙层，防止触摸穿透，默认：false  
       icon: 'success', //图标，支持"success"、"loading"  
-      success: function() {
-        setTimeout(function() {
+      success: function () {
+        setTimeout(function () {
           that.setData({
             pop_detail: false,
             pop_btn: false,
@@ -153,8 +147,8 @@ Page({
           })
         }, 2000)
       }, //接口调用成功
-      fail: function() {}, //接口调用失败的回调函数  
-      complete: function() {} //接口调用结束的回调函数  
+      fail: function () {}, //接口调用失败的回调函数  
+      complete: function () {} //接口调用结束的回调函数  
     })
 
     console.log('关闭')
@@ -162,7 +156,7 @@ Page({
 
 
   //距离运算
-  distance2: function(lat1, lng1, lat2, lng2) {
+  distance2: function (lat1, lng1, lat2, lng2) {
     var _this = this;
     //调用距离计算接口
     qqmapsdk.calculateDistance({
@@ -177,7 +171,7 @@ Page({
         latitude: lat2,
         longitude: lng2
       }], //终点坐标
-      success: function(res) { //成功后的回调
+      success: function (res) { //成功后的回调
         console.log(res);
         var res = res.result;
         var dis = [];
@@ -189,16 +183,16 @@ Page({
         });
         console.log(dis)
       },
-      fail: function(error) {
+      fail: function (error) {
         console.error(error);
       },
-      complete: function(res) {
+      complete: function (res) {
         console.log(res);
       }
     });
 
   },
-  distance: function(lat1, lng1, lat2, lng2) {
+  distance: function (lat1, lng1, lat2, lng2) {
     lat1 = lat1 || 0;
     lng1 = lng1 || 0;
     lat2 = lat2 || 0;
@@ -233,7 +227,7 @@ Page({
   },
 
   //获取参与者地理位置,进行打卡运算
-  setlocationP: function(e) {
+  setlocationP: function (e) {
     var that = this
     var latitude1, latitude2, longitude1, longitude2
     var distance
@@ -243,7 +237,7 @@ Page({
       success(res) {
         console.log(res)
         latitude1 = res.latitude,
-        longitude1 = res.longitude
+          longitude1 = res.longitude
         db.collection('sport').where({
           _id: that.data.sportId
         }).get().then(res => {
@@ -258,8 +252,8 @@ Page({
           wx.showLoading({
             title: '打卡中',
             mask: true,
-            success: function() {
-              setTimeout(function() {
+            success: function () {
+              setTimeout(function () {
                 if (distance < 200) {
                   wx.hideLoading()
 
@@ -267,13 +261,13 @@ Page({
                     _sport_id: that.data.sportId,
                     _openid: app.appData.user_openid
                   }).get({
-                    success: function(res) {
+                    success: function (res) {
                       var participateId = res.data[0]._id
                       console.log(participateId)
 
                       db.collection('Participate').doc(participateId).update({
                         data: {
-                          _picture:that.data.img,
+                          _picture: that.data.img,
                           _if_finished: true
                         },
                         success: res => {
@@ -309,7 +303,7 @@ Page({
 
   },
   //获取发起人地理位置
-  setlocationS: function() {
+  setlocationS: function () {
     var that = this
 
     wx.getLocation({
@@ -323,7 +317,7 @@ Page({
 
       }
     })
-    setTimeout(function() {
+    setTimeout(function () {
       db.collection('sport').doc(that.data.sportId).update({
         data: {
           _latitude: that.data.latitude,
@@ -337,7 +331,7 @@ Page({
   },
 
   //发起打卡
-  startMark: function(e) {
+  startMark: function (e) {
     var that = this
     this.onWatch()
     this.setData({
@@ -388,18 +382,18 @@ Page({
   },
 
   //发起人完成打卡
-  finish: function(e) {
+  finish: function (e) {
     var that = this
     db.collection('Participate').where({
         _sport_id: that.data.sportId
       })
       .watch({
 
-        onChange: function(snapshot) {
+        onChange: function (snapshot) {
 
           that.onShow()
         },
-        onError: function(err) {
+        onError: function (err) {
           console.error('the watch closed because of error', err)
         }
       }).close()
@@ -409,34 +403,37 @@ Page({
     })
   },
 
-    tabbarOnChange: async function(pageInfo){
-        if (pageInfo.detail["title"] =="积分排行榜"){
-            var rc = await db.collection('account_info')
-                .field({
-                    nickName: true,
-                    score: true,
-                    avatar: true
-                })
-                .limit(10)
-                .orderBy('score', 'desc')
-                .get()
-                .catch(err => {
-                    console.log(err)
-                })
-            
-            var curRank = 1
-            for (var item in rc.data){
-                rc.data[item].rankNum = curRank++
-            }
+  tabbarOnChange: async function (pageInfo) {
+    if (pageInfo.detail["title"] == "积分排行榜") {
+      var rc = await db.collection('User')
+        .field({
+          username: true,
+          score: true,
+          avatar: true
+        })
+        .limit(10)
+        .orderBy('score', 'desc')
+        .get()
+        .catch(err => {
+          console.log(111)
+          console.log(err)
+        })
 
-            this.setData({
-                rank: { ...rc.data }
-            })
+      var curRank = 1
+      for (var item in rc.data) {
+        rc.data[item].rankNum = curRank++
+      }
+
+      this.setData({
+        rank: {
+          ...rc.data
         }
-    },
+      })
+    }
+  },
 
   //页面数据更新
-  refresh: function(e) {
+  refresh: function (e) {
     console.log('数据更新')
     var that = this
     db.collection('Participate').where({
@@ -463,65 +460,64 @@ Page({
   },
 
   //参与打卡
-  mark: function(e) {
+  mark: function (e) {
     var that = this
     var length
     var lastTime
     var diff
     var nowTime
-    if(that.data.imgUrlPre != that.data.img)
-    {
+    if (that.data.imgUrlPre != that.data.img) {
 
-    that.formSubmit();
-    that.getTime();
-    
-    db.collection('Participate').doc(that.data.participateId).get({
-      success:res=>{
-        lastTime = new Date(res.data.markTime)
-        console.log('上次'+lastTime)
-        nowTime=new Date().getTime()
-        diff = (nowTime - lastTime)/(1000*60*60)
-        console.log(diff+'小时')
-        if(diff > 8){
-          wx.showLoading({
-            title: '上传中',
-          });
-          db.collection('Participate').doc(that.data.participateId).update({
-            data:{
-              imgs: db.command.push(that.data.imgUrlPre),
-              markTime:that.data.markTime
-            },
-            success:res=>{
-              console.log('打卡更新成功')
-              wx.hideLoading({
-                success: (res) => {},
-              })
-              wx.showToast({
-                title: '打卡成功',
-                icon:'success',
-                
-              })
-            }
-          })
-        }else{
-          wx.showToast({
-            title: '当日请勿重复打卡',
-            icon:'none'
-          })
+      that.formSubmit();
+      that.getTime();
+
+      db.collection('Participate').doc(that.data.participateId).get({
+        success: res => {
+          lastTime = new Date(res.data.markTime)
+          console.log('上次' + lastTime)
+          nowTime = new Date().getTime()
+          diff = (nowTime - lastTime) / (1000 * 60 * 60)
+          console.log(diff + '小时')
+          if (diff > 8) {
+            wx.showLoading({
+              title: '上传中',
+            });
+            db.collection('Participate').doc(that.data.participateId).update({
+              data: {
+                imgs: db.command.push(that.data.imgUrlPre),
+                markTime: that.data.markTime
+              },
+              success: res => {
+                console.log('打卡更新成功')
+                wx.hideLoading({
+                  success: (res) => {},
+                })
+                wx.showToast({
+                  title: '打卡成功',
+                  icon: 'success',
+
+                })
+              }
+            })
+          } else {
+            wx.showToast({
+              title: '当日请勿重复打卡',
+              icon: 'none'
+            })
+          }
         }
-      }
-    })
-    
-  }else{
-    wx.showToast({
-          title: '请点击预览图上传图片',
-          icon:'none',
-    })
-  }
-    
+      })
+
+    } else {
+      wx.showToast({
+        title: '请点击预览图上传图片',
+        icon: 'none',
+      })
+    }
 
 
-    
+
+
     // this.setData({
     //   load_show: true
     // })
@@ -560,16 +556,19 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
     var that = this
     that.formSubmit();
     wx.getSetting({
-      success (res){
+      success(res) {
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称
           wx.getUserInfo({
-            success: function(res) {
+            success: function (res) {
               console.log(res.userInfo)
+              that.setData({
+                userInfo: res.userInfo,
+              })
             }
           })
         }
@@ -577,7 +576,7 @@ Page({
     })
     this.distance2(25.303650, 118.796950, 25.304098, 118.793869)
     wx.getSystemInfo({
-      success: function(res) {
+      success: function (res) {
         console.log('windowHeight: ' + res.windowHeight)
         console.log('windowWidith: ' + res.windowWidth)
         that.setData({
@@ -588,7 +587,7 @@ Page({
     })
     db.collection('Activity').where({
       type: 'offline',
-    
+
     }).get({
       success: res => {
         console.log(res.data)
@@ -624,7 +623,7 @@ Page({
   /**
    * 监听函数
    */
-  onWatch: function() {
+  onWatch: function () {
     var that = this;
     var detailIndex = that.data.detailIndex
     var match_all = that.data.match_all
@@ -634,13 +633,13 @@ Page({
       })
       .watch({
 
-        onChange: function(snapshot) {
+        onChange: function (snapshot) {
           console.log('docs\'s changed events', snapshot.docChanges)
           console.log('query result snapshot after the event', snapshot.docs)
           console.log('is init data', snapshot.type === 'init')
           that.refresh()
         },
-        onError: function(err) {
+        onError: function (err) {
           console.error('the watch closed because of error', err)
         }
       })
@@ -649,42 +648,42 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   },
 
@@ -698,7 +697,7 @@ Page({
     })
   },
   //上传图片
-  doUpload:function(){
+  doUpload: function () {
     let that = this;
     let openid = getApp().globalData.openid || wx.getStorageSync('openid');
     wx.chooseImage({
@@ -713,20 +712,20 @@ Page({
         const name = Math.random() * 1000000;
         const cloudPath = name + filePath.match(/\.[^.]+?$/)[0]
         wx.cloud.uploadFile({
-          cloudPath,//云存储图片名字
-          filePath,//临时路径
+          cloudPath, //云存储图片名字
+          filePath, //临时路径
           success: res => {
             console.log('[上传图片] 成功：', res)
             that.setData({
-              bigImg: res.fileID,//云存储图片路径,可以把这个路径存到集合，要用的时候再取出来
+              bigImg: res.fileID, //云存储图片路径,可以把这个路径存到集合，要用的时候再取出来
             });
             let fileID = res.fileID;
             //把图片存到users集合表
             that.setData({
-              imgUrlPre:fileID,
+              imgUrlPre: fileID,
             })
           },
-           fail: e => {
+          fail: e => {
             console.error('[上传图片] 失败：', e)
           },
           complete: () => {
@@ -736,14 +735,14 @@ Page({
       }
     })
   },
-  getTime:function(){
+  getTime: function () {
     var time = util.formatTime(new Date());
     var that = this
     var lastTime
     var delta
-    console.log("时间"+time)
+    console.log("时间" + time)
     this.setData({
-      markTime:time
+      markTime: time
     })
     // db.collection('Participate').doc(that.data.participateId).get({
     //   success:res=>{
